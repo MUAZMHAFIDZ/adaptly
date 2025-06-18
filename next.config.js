2ba79e0ab65f185a8699e018ff6456f6ef744069
unknown <khu0mmh@gmail.com>
2025-06-18 10:14:14 +0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
   typescript: {
-    ignoreBuildErrors: true, // ✅ ini yang melewatkan error TS saat build
+    ignoreBuildErrors: true,
   },
 };
 
